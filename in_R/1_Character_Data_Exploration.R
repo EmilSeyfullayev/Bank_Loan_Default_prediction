181d18b3ad68f611235855c1dde54abc04489935
Emil Seyfullayev <67646591+EmilSeyfullayev@users.noreply.github.com>
2020-08-25 14:42:42 +0400

    Add files via upload

--- in_R/1_Character_Data_Exploration.R
+++ in_R/1_Character_Data_Exploration.R
@@ -14,69 +14,82 @@ df %>%
   mutate(percentage=round(pcnt,3)) %>% 
   select(col_name, percentage)
 
-#Exclude variables which have more than 50% of missing values
+#We have
+# 22 char var
+# 17 logical var
+# 35 numeric var
+
 df %>% 
-  inspect_na() %>% 
-  as.data.frame() %>% 
-  select(col_name, pcnt) %>% 
-  mutate(percentage=round(pcnt,3)) %>% 
-  select(col_name, percentage) %>% 
-  filter(!(percentage>50)) %>% 
-  .$col_name -> colnames
+  select_if(is.logical) %>% 
+  inspect_na() # all logical variables are 100% NA
 
-df <- df %>% select(colnames)
 
-df %>% ncol() #So we left with 54 variables
+# #Exclude variables which have more than 50% of missing values
+# df %>%
+#   inspect_na() %>%
+#   as.data.frame() %>%
+#   select(col_name, pcnt) %>%
+#   mutate(percentage=round(pcnt,3)) %>%
+#   select(col_name, percentage) %>%
+#   filter(!(percentage>50)) %>%
+#   .$col_name -> colnames
+# 
+# df <- df %>% select(colnames)
+# 
+# df %>% ncol() #So we left with 54 variables
+
+#We will drop logicals at the end
 
 #2 Each character variable----
-write.csv(df, 'df.csv')
-
-df %>% 
-  select(-V1) -> df
 
 df %>% glimpse()
 
 df %>% 
   select_if(is.character) %>% 
-  names() -> char_names ;char_names %>% length() #22
+  names() -> char_names 
+
+char_names %>% length() #22
 
 #1 term-------
 char_names[1]
 df %>% select(char_names[1]) %>% unique() 
 #only two uniques, so, let's make it factorial
+df[,'term'] %>% inspect_na() # no NA
 df$term <- df$term %>% as.factor()
 
 #2 grade-------
 df %>% select(char_names[2]) %>% unique() %>% count()
+df$grade[is.na(df$grade)] # no na
 #only seven uniques, no missing >> factor
 df$grade <- df$grade %>% as.factor()
 
 #3 subgrade------
 df %>% select(char_names[3]) %>% unique() %>% .[[1]]
 df %>% select(char_names[3]) %>% unique() %>% count()
+df %>% select(char_names[3]) %>% inspect_na()
 #no missing, 35 uniques >> factor
 df$sub_grade <- df$sub_grade %>% as.factor()
 
 #4 emp_title---------
-df %>% select(char_names[4]) %>% unique() %>% count() 
+df %>% select(char_names[4]) %>% 
+  unique() %>% 
+  count() 
 #201076 uniques
 df %>% select(char_names[4]) %>% 
   unique() %>%
   .[[1]] %>% 
   .[1:50] #first 50 values
+
 #too many uniqe values, so, drop it
 df %>% 
   select(-char_names[4]) -> df
 
-#5 emp_lenght-------
+#5 emp_length-------
 df %>% select(char_names[5]) %>% unique() %>% count()
 df %>% select(char_names[5]) %>% unique() %>% .[[1]]
 #Uniques are 12, 1 of them is missing
-#Make it factor and
+df[,"emp_length"] %>% inspect_na() # 5% is missing
 #See distribution
-
-
-library(ggplot2)
 
 df$emp_length %>% 
   table() %>% 
@@ -93,64 +106,149 @@ df$emp_length %>%
 #but let's see if any other correlations
 df %>% 
   ggplot(aes(emp_length, loan_amnt))+
-  geom_boxplot()
+  geom_boxplot()+
+  theme_bw()
 
 #As we can see, we could impute with ">10 years" as it is mode
 #But we see that, those have missing "emp_length" have less amount credited
 #So, their emp length shall be as lower as "< 1year"
 #Because their loan amount median is lower than all the rest
 
-df$emp_length %>% unique() %>% .[2] -> y
-df[df$emp_length=="","emp_length"] <- y
+df %>% 
+  group_by(emp_length) %>% 
+  summarise(median(loan_amnt)) %>% 
+  arrange(.[,2])
+#We see that if we take into account loan amount
+#we have to fill NA with < 1 year
+#because it is closest to it value
 
-df$emp_length %>% as.factor() -> df$emp_length
+df$emp_length %>% unique() %>% .[2] -> y
+df[is.na(df$emp_length),"emp_length"] <- y
+
+df$emp_length %>% unique() #no NA
+
+#Since this value is contnous, we have to replace
+#characters and parse numbers
+df$emp_length %>% parse_number()
+#We simply can assign it to the emp_length feature
+#Keep in mind, < 1 year and 1 year will be merged
+#if we look at the median values for
+# <1 year it is 10200
+# 1 year it is 11450
+# not that much difference, but we can change it
+
+#let's create checkpoint for now
+chpoint_1 <- df
+df[df$emp_length=="< 1 year", "emp_length"] <- 0
+df$emp_length %>% unique() #great
+df$emp_length %>% parse_number() %>% unique() #great
+df[, "emp_length"] <- df$emp_length %>% parse_number()
 
 #6 home_ownership--------
 
 df %>% select(char_names[6]) %>% unique() %>% .[[1]]
 df %>% select(char_names[6]) %>% unique() %>% count()
 #number of unique is 6
-#just make them factor
-char_names[6]
+df %>% 
+  group_by(home_ownership) %>% 
+  summarise(n()) %>% 
+  arrange(desc(.[,2]))
+# Other=182, None=50, Any=1, let's combine them
+df[
+  df$home_ownership %in% c('OTHER',
+                        'NONE',
+                        'ANY'),
+  "home_ownership"
+] #total 233, so let's reaasign them to "OTHER"
+df[
+  df$home_ownership %in% c('OTHER',
+                           'NONE',
+                           'ANY'),
+  "home_ownership"
+] <- "OTHER"
+
+df$home_ownership %>% unique() #great
+
+#and just make them factor
 df$home_ownership %>% as.factor() -> df$home_ownership
 
 #7 verification_status------
 df %>% select(char_names[7]) %>% unique() %>% .[[1]]
-#just 3 unique values >> as factor
-char_names[7]
-df$verification_status %>% as.factor() -> df$verification_status
+#just 3 unique values
 
-#8 XXXissue_d -----
+df[,"verification_status"] %>% inspect_na() 
+#no NA
+
+df %>% 
+  group_by(verification_status) %>% 
+  summarise(n())
+# almost equal distribution among three
+# no need for combination
+#just make it as factor
+
+df$verification_status %>% as.factor() -> 
+  df$verification_status
+
+df[, "verification_status"] %>% glimpse() #great
+
+
+#8 issue_d -----
 char_names[8]
 
 df %>% select(char_names[8]) %>% unique() %>% count()
 #91 unique values, day of month and month
-df$issue_d %>% .[1] %>% typeof() #character
+df[,'issue_d'] %>% inspect_na() #no NA
 #We have to transform it to the Date
-#However we do not have Year
+#However we do not have Year (2007-2015 range though)
+#So, just make it factor
+df$issue_d %>% 
+  as.factor() -> df$issue_d
 
-write.csv(df, "df.csv")
+df[,"issue_d"] %>% glimpse()
 
-#9 XXXXloan_status ~ Y variable----
+#9 loan_status ~ Y variable----
 char_names[9]
 df %>% select(char_names[9]) %>% unique() %>% count()
 # number of uniques is 9
 df$loan_status %>% unique()
-#This is actually our Y variable
+#This is actually our Predicted variable
 #It has 9 uniques but will make it to have 2 uniques
+#We have separate them into two
+#Let's make chpoint
+chpoint_2 <- df
 
+# will be encoded as 1 who 
+# Fully Paid, Current, In grace period
+# rest as defaulted
+df$loan_status %>% 
+  unique() %>% 
+  .[c(1, 3, 6)] -> paid_1
 
+df[
+  df$loan_status %in% paid_1,
+  'loan_status'
+] #412111 count
 
+ifelse(
+  df$loan_status %in% paid_1,
+  1,
+  0
+) -> df$loan_status
 
-
+df %>% 
+  filter(loan_status==1) %>% 
+  count() #412111  #great!
 
 
 
 #10 pymnt_plan-------
 char_names[10]
 df$pymnt_plan %>% unique()
+df[, "pymnt_plan"] %>% inspect_na() #no NA
 #has two uniques, "n" and "y" >>> as factor
 df$pymnt_plan <- df$pymnt_plan %>% as.factor()
+df[, "pymnt_plan"] %>% glimpse() #great
+
 #11 URL----
 char_names[11]
 df$url %>% unique() %>% length
@@ -161,8 +259,6 @@ df$url[1] #lets examine one
 df %>% 
   select(-url) -> df
 
-chpoint_1 <- df
-
 #12 desc------
 char_names[12]
 df$desc %>% unique() %>% length()
@@ -171,7 +267,7 @@ df$desc[1]
 df$desc[424774]
 df$desc[1875]
 df$desc %>% class()
-#it is character data type and has huge uniques
+#it has huge number of uniques
 #We will simply drop it
 df %>% 
   select(-desc) -> df
@@ -179,7 +275,7 @@ df %>%
 #13 purpose----
 char_names[13]
 df$purpose %>% unique() %>% length()
-#14 unique values
+#14 unique values, no NA
 df$purpose %>% unique()
 df$purpose %>% 
   table() %>% 
@@ -192,6 +288,13 @@ df$purpose <-
   df$purpose %>% 
   as.factor()
 
+#14 title----
+char_names[14]
+df$title %>% unique() %>% length() #61395 uniques
+#We could process this data, but we need NLP tools to apply
+
+df %>% 
+  select(-title) -> df
 #15 zip_code----
 char_names[15]
 df$zip_code %>% unique() %>% length()
@@ -205,7 +308,7 @@ grepl('xx', df$zip_code, fixed = T) %>%
   sum() %>% 
   .[1]/nrow(df) # =1, means that all zip codes have "xx"
 
-#But it soeas not mean that all have it in the end
+#But it does not mean that all have it in the end
 #How to check it
 #we can use sql language 'like' operator (I simply know it)
 
@@ -217,6 +320,11 @@ sqldf("SELECT zip_code
 nrow(sql_zipcode)/nrow(df) # =1, which means all have "___xx" pattern
 
 #lets remove them and see distribution of zipcodes
+
+df %>% 
+  group_by(zip_code) %>% 
+  summarise(n()) %>% 
+  arrange(.[,2] %>% desc())
 
 gsub("xx",
      "",
@@ -302,33 +410,98 @@ zip_code$name <- zip_code$name %>%
   as.numeric() %>% 
   as.character()
 
+zip_code %>% glimpse()
 #we can merge datasets
 #first of all let's create checkpoint
-chpoint3 <- df
+chpoint_3 <- df
+#also dont forget to make them as factors,
+#otherwise leftjon will work very slowly on joining
 
-merge(df, zip_code,
-      by.x = 'zip_code',
-      by.y = 'name') %>% 
+df$zip_code <- df$zip_code %>% as.factor()
+zip_code$name <- zip_code$name %>% as.factor()
+
+df %>% 
+  left_join(zip_code,
+      by = c('zip_code'='name')) -> df_zip
+
+#now remove Freq and zip_code and rename class
+df_zip %>%
   select(-Freq, -zip_code) %>% 
-  rename(zip_class=class) -> df
+  rename(zip_class=class) -> df_zip
 
+df <- df_zip
+df[, "zip_class"] %>% glimpse() #double >>factor
 df$zip_class <- df$zip_class %>% as.factor()
 
 #16 addr_state----
 char_names[16]
 df$addr_state %>% unique() %>% length()
 #50 unique values
-df$addr_state %>% length() %>% .[1]/nrow(df)
-#no missing values
+df[is.na(df$addr_state),] %>% nrow() #No NAs
+
 #>>> just make it factor 
 df$addr_state <- df$addr_state %>% as.factor()
 
-#17 XXXXearliest_cr_lin------
+#17 earliest_cr_line------
 #date variable
 char_names[17]
 df$earliest_cr_line %>% unique() %>% length()
 # 652 unique values
+df$earliest_cr_line[1] #format(%b-%y)
+#let's paste day date to every value and transform to date
+paste0("01-", df$earliest_cr_line) %>% 
+  as.Date("%d-%b-%y") %>% is.na() %>% sum() #29 NAs
 
+chpoint_4 <- df
+
+df[, "earliest_cr_line"] <- 
+  paste0("01-", df$earliest_cr_line) %>% 
+  as.Date("%d-%b-%y")
+df[, "earliest_cr_line"] %>% glimpse() #date format
+
+#format is corrected
+#let's check nulls
+df[is.na(df$earliest_cr_line), "earliest_cr_line"] #29 NAs
+#It is quite hard to predict date, 
+#we have to simply drop them
+
+df[!is.na(df$earliest_cr_line),] -> df
+
+#Now, in order to use this column we have to transform it 
+#to the continous values
+#We can do it by subtracting from 2017 year
+'2017-01-01' %>% 
+  as.Date() %>% 
+  .[1]-df$earliest_cr_line -> time_diff
+
+time_diff %>% class() #difftime type
+time_diff %>% as.numeric() -> time_diff
+time_diff %>% class()
+time_diff[time_diff<0] %>% length() #1169 have negative value
+#What a big mistake
+#we had 70 years, for example, 
+#we assued them to be 1970
+#bu they were transformed as 2070
+
+#as we have 100 years of mistake we have to simply add them
+100*12*30 -> to_be_added
+time_diff[time_diff<0] <- 
+  time_diff[time_diff<0] + to_be_added
+
+time_diff %>% length() #great
+
+time_diff %>% class() #numeric
+
+#convert to difference in months
+round(time_diff/30, 0) -> time_diff
+chpoint_5 <- df #checkpoint
+df$earliest_cr_line <- time_diff
+
+df[,"earliest_cr_line"] %>% glimpse()
+df[,"earliest_cr_line"] %>% inspect_na() #great
+
+df %>% 
+  rename(Months_past_first_cr=earliest_cr_line)->df
 #18 Initial_list_status------
 char_names[18]
 df$initial_list_status %>% unique() %>% length()
@@ -342,20 +515,28 @@ df[df$initial_list_status=="", "initial_list_status"]
 df$initial_list_status <- 
   df$initial_list_status %>% as.factor()
 
-#19 XXXXlast_pymnt_d -----
+#19 last_pymnt_d -----
 char_names[19]
 df$last_pymnt_d %>% unique() %>% length()
 # just 99 unique values of date_type
+df$last_pymnt_d <- 
+  df$last_pymnt_d %>% as.factor()
 
-#20 XXXXnext_pymnt_d---- 
+#20 next_pymnt_d---- 
 char_names[20]
 df$next_pymnt_d %>% unique() %>% length()
 # 100 unique dates
 
-#21 XXXXlast_credit_pull_d ----
+df$next_pymnt_d <- 
+  df$next_pymnt_d %>% as.factor()
+
+
+#21 last_credit_pull_d ----
 char_names[21]
 df$last_credit_pull_d %>% unique() %>% length()
 # 104 unique values
+df$last_credit_pull_d <- 
+  df$last_credit_pull_d %>% as.factor()
 
 #22 application_type----- 
 char_names[22]
@@ -364,7 +545,23 @@ df$application_type %>% unique() %>% length()
 df %>% 
   select(-application_type) -> df
 
-chpoint <- df
+chpoint_6 <- df
+
+df %>% 
+  select_if(is.logical) %>%
+  .[complete.cases(.),] # no datatable to show
+#so, simply drop logical variables
+
+df %>% 
+  select_if(is.logical) %>% 
+  colnames() -> log_names #17 NAs >> 70-17=53 
+#we should get 53 columns at the end
+
+df %>% 
+  select(-log_names) -> df
+
+chpoint_7 <- df
+write.csv(df, "df.csv")
 
 
 
--- in_R/2_Numeric_Data_Exploration.R
+++ in_R/2_Numeric_Data_Exploration.R
@@ -5,26 +5,62 @@ library(inspectdf)
 df %>% 
   select_if(is.numeric) %>% 
   names() %>% 
-  length() 
+  length() #38 numeric
 
 df %>% 
   select_if(is.character) %>% 
   names() %>%
-  length() 
+  length() # no character
 
 df %>% 
   select_if(is.factor) %>% 
   names() %>% 
-  length()
+  length() #14 factors (already processed)
 
 df %>% 
   select_if(is.numeric) %>% 
-  colnames() -> numnames
+  colnames() -> num_names
 
-numnames
+num_names
+
+# remove id -----
+df$id %>% unique() %>% length()
+#466256 uniques >> drop
+df %>% 
+  select(-id) -> df
+
+# remove member_ID----
+num_names[2]
+df$member_id %>% unique() %>% length()
+# drop simply
+df %>% 
+  select(-member_id) -> df
+
+
+#Till now most of the values are not NA
+#Lets focus on them
+#some have > 50% of Missing, lets drop them
+
+df %>% 
+  select_if(is.numeric) %>% 
+  inspect_na() %>% 
+  as.data.frame() %>% 
+  filter(pcnt>50) %>% 
+  .[[1]] -> fiftyPcntMissing
+
+df %>% 
+  select(-fiftyPcntMissing) -> df
+
+df %>% 
+  select_if(is.numeric) %>% 
+  inspect_na() %>% 
+  filter(pcnt>0) %>% 
+  .[[1]] -> num_having_missing
+
+# the highest has 15% of missing
 
 #1 tot_coll_amt-----
-numnames[1]
+num_having_missing[1]
 df$tot_coll_amt %>% unique() %>% length()
 # 6322 uniques
 df %>% 
@@ -49,7 +85,7 @@ df$tot_coll_amt %>% na.omit() %>% sort(decreasing = T)
 df %>% 
   arrange(desc(tot_coll_amt)) %>% 
   filter((tot_coll_amt==0)) %>% 
-  count() #49750 values are non zero
+  count() #49750 values are non zero from 466256
 #let's see their distribution
 
 df %>% 
@@ -95,31 +131,34 @@ collat <- c()
 
 for (i in df_tot_coll_amt$tot_coll_amt){
   if (i>=9000000){
-    collat <- append(collat, 9)
+    collat <- append(collat, 10)
   }else if(i>=8000000 & i<9000000){
-    collat <- append(collat,8)
+    collat <- append(collat,9)
   }else if(i>=7000000 & i<8000000){
-    collat <- append(collat,7)
+    collat <- append(collat,8)
   }else if(i>=6000000 & i<7000000){
-    collat <- append(collat,6)
+    collat <- append(collat,7)
   }else if(i>=5000000 & i<6000000){
-    collat <- append(collat,5)
+    collat <- append(collat,6)
   }else if(i>=4000000 & i<5000000){
-    collat <- append(collat,4)
+    collat <- append(collat,5)
   }else if(i>=3000000 & i<4000000){
-    collat <- append(collat,3)
+    collat <- append(collat,4)
   }else if(i>=2000000 & i<3000000){
-    collat <- append(collat,2)
+    collat <- append(collat,3)
   }else if(i>=1000000 & i<2000000){
-    collat <- append(collat,1)
+    collat <- append(collat,2)
   }else{
-    collat <- append(collat,0.5)
+    collat <- append(collat,1)
   }
 }
 
 collat %>% table() %>% data.frame()
-# we have just one value with index 9
-# all the rest with index 0.5
+# we have just one value with index 10
+df %>% 
+  filter(tot_coll_amt>9000000) %>% 
+  select(tot_coll_amt) #Its value is 9,152,545
+# all the rest with index 1
 # so, now I think we will be able to access distribution
 
 df_tot_coll_amt %>% 
@@ -142,7 +181,422 @@ df_tot_coll_amt %>%
   ggplot(aes(tot_coll_amt))+
   geom_histogram(bins = 450) #great
 
+#We will reassign those values to the upper and lowe fences
+
+library(graphics)
+boxplot(df$tot_coll_amt)$out %>% 
+  sort(decreasing = F)
+
+up_fence <- 
+  quantile(df_tot_coll_amt$tot_coll_amt,
+           0.75, na.rm = T)+
+  IQR(df_tot_coll_amt$tot_coll_amt,
+      na.rm = T)
+low_fence <- 
+  quantile(df_tot_coll_amt$tot_coll_amt, 
+           0.25, na.rm = T)-
+  IQR(df_tot_coll_amt$tot_coll_amt, na.rm = T)
+
+#Dont forget that we deal only with non-zeros, 50,000
+#Actually, if 415,000 observations are zeros
+#No meaning left of value change
+#Let's simply reassign them 0 for zeros and NAs
+#And 1s for amounts
+
+ifelse(
+  df$tot_coll_amt %in% c(0, NA),
+  0,
+  1
+) -> df$tot_coll_amt
+
+chpoint_8 <- df
+
+#2 tot_cur_bal----
+num_having_missing[2]
+df[,"tot_cur_bal"] %>% inspect_na() #15% missing
+df$tot_cur_bal %>% unique() %>% length() 
+#220691 uniques >> continous
+
+df %>% 
+  ggplot(aes(tot_cur_bal))+
+  geom_histogram(color='black',
+                 fill='red',
+                 bins = 450)+
+  theme_bw()
+
+df$tot_cur_bal %>% max(na.rm = T) #8,000,0078
+df$tot_coll_amt %>% max(na.rm = T) #1
+df$tot_coll_amt %>% median(na.rm = T) # 0
+df$tot_coll_amt %>% mean(na.rm = T) # 0,107
+
+#let's reassign outliers to lower and upper fences
+
+up_fence <- 
+  quantile(df$tot_cur_bal,na.rm = T, 0.75)+
+  IQR(df$tot_cur_bal,na.rm = T)*1.5 #479455.5
+
+low_fence <- 
+  quantile(df$tot_cur_bal, na.rm = T, 0.25)-
+  IQR(df$tot_cur_bal, na.rm = T)*1.5 #-241884
+#since min value is 0, 
+#we will not reassign lower fence outliers
+
+library(graphics)
+
+outliers <-  boxplot(df$tot_cur_bal)$out
+outliers %>% length()
+#length of otliers is 12,264
+df[
+  df$tot_cur_bal %in% outliers,
+  'tot_cur_bal'
+] %>% count()
+#count of outliers  is 12264 
+#Great
+#Now simply reassign
+df[
+  df$tot_cur_bal %in% outliers,
+  'tot_cur_bal'
+] <- up_fence
+
+df %>% 
+  ggplot(aes(tot_cur_bal))+
+  geom_histogram(color='black',
+                 fill='red',
+                 bins=450)+
+  theme_bw()
+
+#let's check correlation with loan_amnt
+cor(
+  df$loan_amnt,
+  df$tot_cur_bal %>% 
+    nafill(fill = 
+              median(
+                df$tot_cur_bal,
+                na.rm = T
+              ))
+) #0.333 >> there is a positive correlation
+
+#let's check with loan_status borrower
+
+df %>% 
+  ggplot(aes(loan_status %>% as.factor(),
+             tot_cur_bal))+
+  geom_boxplot()+
+  theme_bw()
+#median of  tot_cur_balance 
+#of those who paid their loans is higher
+
+#let's check according to grades
+df %>% 
+  ggplot(aes(grade, tot_cur_bal))+
+  geom_boxplot()+
+  theme_bw() #there are quite big differences
+
+#let's see whether na's are more in certain grade
+df[
+  is.na(df$tot_cur_bal),
+  grade
+] %>% table() %>% as.data.frame()
+#yes, there is big portion of NAs in A, B, C grades
+
+#so, we can fill NAs according to
+#loan status and grade
+
+df %>% 
+  group_by(grade, loan_status) %>% 
+  summarise(median(tot_cur_bal %>% 
+                     na.omit())) %>%
+  as.data.frame() %>% 
+  rename(median=colnames(.)[3])
+
+# grade loan_status   median
+# 1      A           0 125116.0
+# 2      A           1 145391.0
+# 3      B           0  63535.0
+# 4      B           1  85779.0
+# 5      C           0  59706.0
+# 6      C           1  73487.0
+# 7      D           0  53025.0
+# 8      D           1  65870.0
+# 9      E           0  62033.0
+# 10     E           1  71245.5
+# 11     F           0  57286.0
+# 12     F           1  65206.0
+# 13     G           0  71562.0
+# 14     G           1  81001.0
+
+df[is.na(df$tot_cur_bal) &
+     grade =="A" &
+     loan_status==0,
+     "tot_cur_bal"
+   ] <- 125116
+
+df[is.na(df$tot_cur_bal) &
+     grade =="A" &
+     loan_status==1,
+   "tot_cur_bal"
+] <- 145391
+
+df[is.na(df$tot_cur_bal) &
+     grade =="B" &
+     loan_status==0,
+   "tot_cur_bal"
+] <- 63535
+
+df[is.na(df$tot_cur_bal) &
+     grade =="B" &
+     loan_status==1,
+   "tot_cur_bal"
+] <- 85779
+
+df[is.na(df$tot_cur_bal) &
+     grade =="C" &
+     loan_status==0,
+   "tot_cur_bal"
+] <- 59706
+
+df[is.na(df$tot_cur_bal) &
+     grade =="C" &
+     loan_status==1,
+   "tot_cur_bal"
+] <- 73487
+
+#the rest will be filled with median
+med <- df$tot_cur_bal %>% 
+  median(na.rm = T) #85779
+
+df[
+  df$tot_cur_bal %>% is.na(),
+  "tot_cur_bal"
+] <- med
+
+df[,"tot_cur_bal"] %>% inspect_na() #no NAs
+
+df %>% 
+  ggplot(aes(tot_cur_bal))+
+  geom_histogram(color='black', 
+                 fill='red')
+
+#3 total_rev_hi_lim-----
+num_having_missing[3]
+df[, "total_rev_hi_lim"] %>% inspect_na() 
+#15% missing
+df$total_rev_hi_lim %>% unique() %>% length()
+#14613 uniques, not to be discrete
+df$total_rev_hi_lim[345678] #example
+df$total_rev_hi_lim[248678] #example
+
+#let's see distribution
+df %>% 
+  ggplot(aes(total_rev_hi_lim))+
+  geom_histogram() # alot of outliers
+
+boxplot(df$total_rev_hi_lim)$out -> outliers
+
+min(df$total_rev_hi_lim, na.rm = T) # 0
+
+# chpoint_9 <- df
+df <- chpoint_9
+
+up_fence <- 
+  quantile(df$total_rev_hi_lim,0.75, na.rm = T)+
+  IQR(df$total_rev_hi_lim, na.rm = T) #62,300
+  
+#reassign outliers to upperfence
+df[
+  df$total_rev_hi_lim %in% outliers,
+  "total_rev_hi_lim"
+] <- up_fence 
+
+#see NAs
+df[df$total_rev_hi_lim %>% is.na(),] %>% 
+  group_by(grade) %>% 
+  summarise(n()) %>% 
+  rename(NA_count=names(.)[2]) %>% 
+  as.data.frame()
+
+# grade NA_count
+# 1     A    16735
+# 2     B    21778
+# 3     C    14539
+# 4     D     9578
+# 5     E     5013
+# 6     F     1944
+# 7     G      660
+
+#Again, NA count is more in A, B, C grades
+
+df[!is.na(df$total_rev_hi_lim),] %>% 
+  ggplot(aes(grade, total_rev_hi_lim))+
+  geom_boxplot()+
+  facet_grid(loan_status~.)+
+  theme_bw()
+
+# Again same type of situation
+# If grade is higher, example A,
+# total_rev_hi_lim is higher
+# and if loan_status is 1 total_rev_hi_lim 
+# is higher than if loan_status 0
+
+df %>% 
+  group_by(grade, loan_status) %>% 
+  summarise(median(total_rev_hi_lim,
+                   na.rm = T)) %>% 
+  as.data.frame() %>% 
+  rename(median=names(.)[3])
+
+# grade loan_status median
+# 1      A           0  31000
+# 2      A           1  34900
+# 3      B           0  22300
+# 4      B           1  23500
+# 5      C           0  20100
+# 6      C           1  20900
+# 7      D           0  18400
+# 8      D           1  19600
+# 9      E           0  20300
+# 10     E           1  20300
+# 11     F           0  19900
+# 12     F           1  19700
+# 13     G           0  19150
+# 14     G           1  21800
+
+df[is.na(total_rev_hi_lim)&
+     grade=="A" &
+     loan_status==0,
+   "total_rev_hi_lim"] <- 31000
+
+df[is.na(total_rev_hi_lim)&
+     grade=="A" &
+     loan_status==1,
+   "total_rev_hi_lim"] <- 34900
+
+df[is.na(total_rev_hi_lim)&
+     grade=="B" &
+     loan_status==0,
+   "total_rev_hi_lim"] <- 22300
+
+df[is.na(total_rev_hi_lim)&
+     grade=="B" &
+     loan_status==1,
+   "total_rev_hi_lim"] <- 23500
+
+df[is.na(total_rev_hi_lim)&
+     grade=="C" &
+     loan_status==0,
+   "total_rev_hi_lim"] <- 20100
+
+df[is.na(total_rev_hi_lim)&
+     grade=="C" &
+     loan_status==1,
+   "total_rev_hi_lim"] <- 20900
+
+df[is.na(total_rev_hi_lim)&
+     grade=="D" &
+     loan_status==0,
+   "total_rev_hi_lim"] <- 18400
+
+df[is.na(total_rev_hi_lim)&
+     grade=="D" &
+     loan_status==1,
+   "total_rev_hi_lim"] <- 19600 
+
+df[is.na(total_rev_hi_lim)&
+     grade=="E" &
+     loan_status==0,
+   "total_rev_hi_lim"] <- 20300
+
+df[is.na(total_rev_hi_lim)&
+     grade=="E" &
+     loan_status==1,
+   "total_rev_hi_lim"] <- 20300 
+
+df[is.na(total_rev_hi_lim)&
+     grade=="F" &
+     loan_status==0,
+   "total_rev_hi_lim"] <- 19900 
+
+df[is.na(total_rev_hi_lim)&
+     grade=="F" &
+     loan_status==1,
+   "total_rev_hi_lim"] <- 19700
+
+df[is.na(total_rev_hi_lim)&
+     grade=="G" &
+     loan_status==0,
+   "total_rev_hi_lim"] <- 19150 
+
+df[is.na(total_rev_hi_lim)&
+     grade=="G" &
+     loan_status==1,
+   "total_rev_hi_lim"] <- 21800
+  
+df[, "total_rev_hi_lim"] %>% inspect_na()
+#Great
+
+#4 revol_util------
+num_having_missing[4]
+df[, "revol_util"] %>% inspect_na() #0.667%
+
+df$revol_util %>% unique() %>% length()
+#1270 uniques
+
+df %>% 
+  ggplot(aes(loan_status %>% as.factor(),
+             revol_util))+
+  geom_boxplot() -> p
+#as we cannot see boxes, 
+#let's see with plotly
+library(plotly)
+p %>% ggplotly()
+# median for 0 loan_status: 61,30
+# median for 1 loan_status: 57,10
+#let's fill accordingly
+df[
+  df$revol_util %>% is.na() &
+    df$loan_status==0,
+  "revol_util"
+] <- 61.30
+
+df[
+  df$revol_util %>% is.na() &
+    df$loan_status==1,
+  "revol_util"
+] <- 57.10
 
 
+df[,"revol_util"] %>% inspect_na()#great
 
+#5 collections_12_mths_ex_med------
+num_having_missing[5]
 
+df$collections_12_mths_ex_med %>% unique()
+#even though there are few uniques,
+#variable is continous
+
+df %>% 
+  ggplot(aes(loan_status %>% as.factor(),
+             collections_12_mths_ex_med))+
+  geom_boxplot() -> p
+
+#as we cannot see boxes, let's see with plotly
+p %>% ggplotly()
+# most of the collections = 0
+
+median(df$collections_12_mths_ex_med, 
+       na.rm = T) # yes, = 0
+mean(df$collections_12_mths_ex_med, 
+       na.rm = T) # almost zero, = 0,009
+
+nafill(df$collections_12_mths_ex_med,
+       fill = 0) %>% is.na() %>% sum() 
+#nafill works
+df$collections_12_mths_ex_med <- 
+  nafill(df$collections_12_mths_ex_med,
+         fill = 0)
+
+df[,"collections_12_mths_ex_med"] %>% 
+  inspect_na() #Great
+
+#chpoint_10 <- df
+df <- chpoint_10
